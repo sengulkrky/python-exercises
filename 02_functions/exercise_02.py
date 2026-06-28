4c7d14ba8c34a0f4b0a3db61b72716270eeda15b
sengulk <sengul.karakaya@student.odisee.be>
2026-06-28 03:05:25 +0200

    exercise 02 - default parameters

---
+++ 02_functions/exercise_02.py
@@ -0,0 +1,5 @@
+def introduce(name, age, country="Belgium"):
+    return f"Hi, I'm {name}, {age} years old and I'm from {country}"
+
+print(introduce("Sengül", 23))
+print(introduce("Sengül", 23, "France"))
