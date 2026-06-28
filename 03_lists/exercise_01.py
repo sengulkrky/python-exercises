102266ff9214edf90d6cdd573366115563d8ec37
sengulk <sengul.karakaya@student.odisee.be>
2026-06-28 03:12:21 +0200

    exercise 01 - lists

---
+++ 03_lists/exercise_01.py
@@ -0,0 +1,15 @@
+# make a list
+movies = ["The Dark Knight", "Inception", "Interstellar", "Obsession", "Shrek 1"]
+
+# print first item
+print(movies[0])
+
+# print last item
+print(movies[-1])
+
+# add new movies
+movies.append("Toy Story 4")
+
+# sort and print list
+movies.sort()
+print(movies)
