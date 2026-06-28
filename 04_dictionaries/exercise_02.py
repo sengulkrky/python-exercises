7d4e4f1a1caba97e1e2c34206e0a0229d6e57aaa
sengulk <sengul.karakaya@student.odisee.be>
2026-06-28 03:28:46 +0200

    exercise 02 - loops with dictionaries

---
+++ 04_dictionaries/exercise_02.py
@@ -0,0 +1,20 @@
+people = [
+    {
+        "name": "Sengül",
+        "age": 23,
+        "city": "Ghent"
+    },
+    {
+        "name": "Princia",
+        "age": 21,
+        "city": "Brussels"
+    },
+    {
+        "name": "Seyda",
+        "age": 21,
+        "city": "Ghent"
+    }
+]
+
+for person in people:
+    print(f"Name: {person['name']} | Age: {person['age']} | City: {person['city']}")
