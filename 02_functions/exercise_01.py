8840322e135d0794df6d3d8189ff696d52233a86
sengulk <sengul.karakaya@student.odisee.be>
2026-06-28 03:02:35 +0200

    exercise 01 - functions

---
+++ 02_functions/exercise_01.py
@@ -0,0 +1,13 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+def is_even(number):
+    return number % 2 == 0
+
+print(greet("Sengül"))
+print(add(1, 10))
+print(is_even(15))
+print(is_even(10))
