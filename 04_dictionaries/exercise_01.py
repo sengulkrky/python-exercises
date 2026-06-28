dc22cce9cc20022bd8df4f94168c3784c89d2d77
sengulk <sengul.karakaya@student.odisee.be>
2026-06-28 03:23:54 +0200

    exercise 01 - dictionaries

---
+++ 04_dictionaries/exercise_01.py
@@ -0,0 +1,22 @@
+person = {
+    "name": "Sengül",
+    "age": 23,
+    "city": "Ghent",
+    "job": "Student"
+}
+
+# print dictionary
+print(person)
+
+# print only name & city
+print(person["name"])
+print(person["city"])
+
+# change city
+person["city"] = "Brussels"
+
+# add email key
+person["email"] = "sengul.krky03@gmail.com"
+
+# print updated version
+print(person)
